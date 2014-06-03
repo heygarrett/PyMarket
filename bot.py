95ce0e7e3db92940884ee0c6c3ee41b543502f1f
Garrett Oreilly <garrettoreilly@icloud.com>
2014-06-03 14:10:56 -0400

    Changed some things around

--- bot.py
+++ bot.py
@@ -1,12 +1,9 @@
-import socket, re, db
+import re, irc, db
 
-class Pymarket(object):
+class Pymarket:
 
-    def __init__(self, host, port, name, channel):
-        self.host = host
-        self.port = port
-        self.name = name
-        self.channel = channel
+    def __init__(self, connection):
+        self.irc = connection
         self.users = []
         self.handlers = {
                 'PRIVMSG': self.message,
@@ -18,80 +15,46 @@ class Pymarket(object):
                 '353': self.names
         }
 
-    def send(self, *data):
-        line = ' '.join(data) + '\r\n'
-        self.irc.send(line.encode())
-
-    def connect(self):
-        self.irc = socket.socket()
-        self.irc.connect((self.host, self.port))
-        self.send('NICK', self.name)
-        self.send('USER', self.name, '8', '*', self.name)
-        self.send('JOIN', self.channel)
-
     def parse_message(self, line):
-        sections = line.split(':')
-        if sections[0] == '':
-            keys = ['sender', 'type', 'target']
-            serv_message = dict((key, value) for key, value in zip(keys, sections[1].split()))
-            if '!' in serv_message['sender']:
-                serv_message['sender'] = serv_message['sender'][0:serv_message['sender'].index('!')]
-            if serv_message['type'] == 'PRIVMSG':
-                serv_message['text'] = ':'.join(sections[2:])
-            if serv_message['type'] == '353':
-                c = re.compile('[^\\w -]')
-                serv_message['nicks'] = c.sub('', sections[2]).split()
-
-            choice = self.handlers.get(serv_message['type'])
+        values = {}
+        if line[0] == ':' and '!' in line:
+            prefix, line = line[1:].split(' ', 1)
+            values['nick'] = prefix.split('!')[0]
+            values['command'], values['target'], line = line.split(' ', 2)
+        if ' :' in line:
+            values['text'] = line.split(' :')
+        if len(values) == 4:
+            choice = self.handlers.get(values['command'])
             if choice:
-               choice(serv_message)
-               
-        elif sections[0] == 'PING':
-            self.send('PONG :%s\r\n' % sections[1])
+                choice(values)
 
-    def message(self, sm):
-        nick = sm['sender']
-        text = sm['text'].split()
+    def message(self, values):
+        print(values['nick'] + ': ' + values['text'])
 
-        for user in self.users:
-            if user in text[0] and text[0].index(user) == 0:
-                receiver = user
-                print('receiver:', receiver)
-                suffix = text[0].replace(user, '')
-                credits = rg.match('^\\+=(\\d+)?$', suffix).group(1)
-                print('credits:', credits)
-                break
+    def join(self, values):
+        self.users.append(values['nick'])
+        print('%s joined %s' % (values['nick'], values['target']))
 
-        if receiver and credits:
-            db.transfer(nick, receiver, credits)
-            self.send('Credits transferred from', nick, 'to', receiver, ':', credits)
-
-    def join(self, sm):
-        self.users.append(sm['sender'])
-        print('%s joined %s' % (sm['sender'], sm['target']))
-
-    def leave(self, sm):
-        self.users.remove(sm['sender'])
-        print('%s left' % sm['sender'])
+    def leave(self, values):
+        self.users.remove(values['nick'])
+        print('%s left' % values['nick'])
         
-    def names(self, sm):
-        self.users += sm['nicks']
+    def names(self, values):
+        self.users += values['nick']
         print(self.users)
         for i in self.users:
             if not db.checkBal(i):
                 db.addAcc(i)
 
 def main():
-    client = Pymarket('irc.freenode.net', 6667, 'PyMarket', '#gyaretto')
-    client.connect()
-    buf = ''
+    connection = irc.Irc('irc.freenode.net', 6667, 'PyMarket', '#gyaretto')
+    bot = Pymarket(connection)
+    connection.connect()
     while True:
-        buf += client.irc.recv(4096).decode()
-        if '\r\n' in buf:
-            line = buf[:buf.index('\r\n')]
-            buf = buf[buf.index('\r\n') + 1:]
-            client.parse_message(line)
+        line = connection.receive()
+        if line:
             print(line)
+            bot.parse_message(line)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ irc.py
@@ -0,0 +1,27 @@
+import socket
+
+class Irc:
+
+    def __init__(self, host, port, name, channel):
+        self.host = host
+        self.port = port
+        self.name = name
+        self.channel = channel
+        self.buf = ''
+        self.client = socket.socket()
+
+    def connect(self):
+        self.client.connect((self.host, self.port))
+        self.send('NICK', self.name)
+        self.send('USER', self.name, '8', '*', self.name)
+        self.send('JOIN', self.channel)
+
+    def send(self, *data):
+        line = ' '.join(data) + '\r\n'
+        self.client.send(line.encode())
+
+    def receive(self):
+        self.buf += self.client.recv(4096).decode()
+        if '\r\n' in self.buf:
+            line, self.buf = self.buf.split('\r\n', 1)
+            return line
