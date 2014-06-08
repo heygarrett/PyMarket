34a86d832b1a531359508c9eaa1572b9771041a5
Garrett Oreilly <garrettoreilly@icloud.com>
2014-06-07 22:56:42 -0400

    Forgot condition of nick credit check not existing

--- bot.py
+++ bot.py
@@ -71,7 +71,10 @@ class Pymarket:
     def notice(self, values):
         if db.checkBal(values['text']):
             self.irc.send('NOTICE', values['nick'], ':' + \
-                    values['text'], '=>', str(db.checkBal(values['text'])))
+                    values['text'], 'has', str(db.checkBal(values['text'])), 'credits.')
+        else:
+            self.irc.send('NOTICE', values['nick'], ':' + \
+                    'Sorry, but', values['text'], 'does not have an account.')
 
     def join(self, values):
         self.users.add(values['nick'])
